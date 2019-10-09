4632a95709bff8e36efbbafc5eb3686e933c31fc
Cassius Souza <cassius.sanches@hotmail.com>
2019-10-08 21:50:38 -0300

    Finishing Go Barber API, Create User, Login, JWT

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,9 +15,9 @@ module.exports = {
   },
   rules: {
     "prettier/prettier": "error",
-    'class-method-use-this': 'off',
+    'class-methods-use-this': 'off',
     'no-param-reassign': 'off',
     'camelcase': 'off',
-    'no-unused-vars': ['error', { 'argsIgnorePattern': 'next' }],    
+    'no-unused-vars': ['error', { 'argsIgnorePattern': 'next' }],
   }
 };
